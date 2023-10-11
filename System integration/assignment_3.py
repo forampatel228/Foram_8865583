62ff26e14c7e7722e82fbb3281f614903d7f4e30
foram_8865583 <Fpatel5583@conestogac.on.ca>
2023-10-10 21:37:15 -0400

    file added in assignment folder

--- System integration/assignment_3.py
+++ System integration/assignment_3.py
@@ -1,63 +1,52 @@
-
 # Importing required libraries
 
 # pip install selenium
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
 import time
 
 # Setting up the webdriver
 driver = webdriver.Chrome()
 
 # Navigating to the Amazon.ca homepage
-driver.get("https://www2.hm.com/en_ca/index.html")
-time.sleep(3)
+driver.get("https://www.amazon.ca/")
+time.sleep(10)
 
 # Finding the search bar and entering text
 # search_bar = driver.find_element_by_id("id","twotabsearchtextbox") old syntax
-#search_bar = driver.find_element("id","twotabsearchtextbox")
-#search_bar.send_keys("laptop")
+all_button = driver.find_element("xpath","/html/body/div[1]/header/div/div[4]/div[1]/a")
+all_button.click()
 
-# Submitting the search query
-search_bar.send_keys(Keys.RETURN)
-
-# Waiting for the search results page to load
-#time.sleep(5)
-
-# Verifying that the search results page has loaded
-#assert "laptop" in driver.title
-
-# Selecting a laptop from the search results
-#laptop_link = driver.find_element("xpath","/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/span/a/div/img")
-#laptop_link.click()
+time.sleep(10)
+#best seller button click bt xpath
+bestseller_button = driver.find_element("xpath","/html/body/div[3]/div[2]/div/ul[1]/li[2]/a")
+bestseller_button.click()
 
 
+time.sleep(10)
+product_image =  driver.find_element("id","B01N17N0MO")
+product_image.click()
 
-# Waiting for the laptop details page to load
-#time.sleep(5)
+time.sleep(10)
 
-# Adding the laptop to the cart
-#add_to_cart_button = driver.find_element("id","add-to-cart-button")
-#add_to_cart_button.click()
+#filter button click by id
+add_cart_button =  driver.find_element("id","add-to-cart-button")
+add_cart_button.click()
+time.sleep(10)
 
-# Waiting for the cart to update
-#time.sleep(5)
+#go to card button click css selector
+goto_cart_button = driver.find_element(By.CSS_SELECTOR, "#sw-gtc > span > a")
+goto_cart_button.click()
+time.sleep(10)
 
-# Clicking on no thanks button
-#no_thanks_button= driver.find_element("xpath","/html/body/div[9]/div[3]/div[1]/div/div/div[2]/div[2]/div/div/div[3]/div/span[2]/span/input")
-#no_thanks_button.click()
-#time.sleep(2)
+#proceed to checkout button click
+proceed_to_checkout = driver.find_element(By.CLASS_NAME, "a-button-inner")
+proceed_to_checkout.click()
+time.sleep(10)
 
-#proceed_to_checkout= driver.find_element("xpath","/html/body/div[1]/div[1]/div/div[1]/div[2]/div/div[3]/div/div[1]/form/span/span/span/input")
-#proceed_to_checkout.click()
-#time.sleep(2)
-
-
-# Verifying that the laptop has been added to the cart
-# cart_count = driver.find_element("id","nav-cart-count")
-# assert cart_count.text == "1"
-# cart_count.click()
 
 # Closing the webdriver
 driver.close()
